87a83fca7dee62153ae9469b94ab79fdcf5e4601
jeronimocabanelas-maker <jeronimo.cabanelas@aluno.senai.br>
2026-05-05 10:44:49 -0500

    novoo

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-const campoBusca = document.getElementById('campoBusca')
-const btnBuscar = document.getElementById('btnBuscar')
-const resultado = document.getElementById('resultado')
-const msgErro = document.getElementById('msgErro')
+const campoBusca = document.getElementById('campoBusca');
+const btnBuscar  = document.getElementById('btnBuscar');
+const resultado  = document.getElementById('resultado');
+const msgErro    = document.getElementById('msgErro');
+
 
 async function buscarPokemon() {
-    const nome = campoBusca.ariaValueMax.toLowerCase().trim();}
+    const nome = campoBusca.value.toLowerCase().trim();
 
-
-    if(nome === '') {
-        alert('Digite o nome de um pokémon!');
+   
+    if (nome === '') {
+        alert('Digite o nome de um Pokémon!');
         return;
     }
 
     try {
-        const resposta = await fetch('https://pokeapi.co/api/v2/pokemon/$(nome)');
-        if(!resposta.ok) throw new error('não encontrado');
         
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`);
 
+        
+        if (!resposta.ok) throw new Error('Não encontrado');
+
+        
         const dados = await resposta.json();
+
+        
+        document.getElementById('pokeNome').textContent  = dados.name;
+        document.getElementById('pokeId').textContent    = dados.id;
+        document.getElementById('pokeAltura').textContent = dados.height;
+        document.getElementById('pokePeso').textContent  = dados.weight;
+        document.getElementById('pokeTipo').textContent  = dados.types[0].type.name;
+        document.getElementById('pokeImg').src           = dados.sprites.front_default;
+
+      
+        resultado.classList.remove('escondido');
+        msgErro.classList.add('escondido');
+
+    } catch (erro) {
+        
+        resultado.classList.add('escondido');
+        msgErro.classList.remove('escondido');
     }
+}
+
+
+btnBuscar.addEventListener('click', buscarPokemon);
+
+
+campoBusca.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') buscarPokemon();
+});
